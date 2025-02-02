230da871bd586dcbaef90079affbc46dfa5d8de0
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-02-02 16:38:51 -0300

    Fix FRED params

--- data.php
+++ data.php
@@ -2,7 +2,7 @@
 $url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?';
 $params = array(
     'mode' => 'fred',
-    'id' => 'DSG' . $_GET['maturity'],
+    'id' => 'DGS' . $_GET['maturity'],
     'fq' => 'Daily',
     'cosd' => '2019-01-01',
     'coed' => date('Y-m-d')
