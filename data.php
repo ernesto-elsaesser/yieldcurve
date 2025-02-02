2becafb563c0e47007ead5e06fbc58acdd531b8a
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-02-02 15:47:05 -0300

    Fix PHP query

--- data.php
+++ data.php
@@ -1,4 +1,11 @@
 <?php
-$url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&fq=Daily&cosd=2019-01-01&coed=' . date('Y-m-d') . '&id=DSG' . $_GET['maturity'];
+$params = array(
+    'mode' => 'fred',
+    'fq' => 'Daily',
+    'cosd' => '2019-01-01',
+    'coed' => date('Y-m-d'),
+    'id' => 'DSG' . $_GET['maturity']
+);
+$url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?' . http_build_query($params, '', '&');
 echo file_get_contents($url);
 ?>
