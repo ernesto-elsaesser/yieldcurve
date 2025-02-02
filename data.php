8b2086a39fbe6a750b25230622fb35d57440f112
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-02-02 15:35:33 -0300

    Fix PHP request

--- data.php
+++ data.php
@@ -1,6 +1,4 @@
 <?php
-$id = "DSG" . $_GET['maturity'];
-$today = date("%Y-%m-%d");
-$url = "https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=$id&fq=Daily&cosd=2019-01-01&coed=$today";
+$url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&fq=Daily&cosd=2019-01-01&coed=' . date('Y-m-d') . '&id=DSG' . $_GET['maturity'];
 echo file_get_contents($url);
 ?>
